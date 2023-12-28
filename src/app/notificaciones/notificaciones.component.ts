cc483d3e579863d067ece1446f8b3c35cb6e85cf
Agus2309 <agus.cavia.3@gmail.com>
2023-12-28 17:01:10 -0300

    Estilo notificaciones

--- src/app/notificaciones/notificaciones.component.ts
+++ src/app/notificaciones/notificaciones.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-notificaciones',
@@ -7,18 +7,62 @@ import { Router } from '@angular/router';
   styleUrls: ['./notificaciones.component.css']
 })
 export class NotificacionesComponent implements OnInit {
-  notificaciones: Notificacion[] = [];
-  constructor(private router: Router) {}
+  notificaciones: Notificacion[] = []
+  contadorNotificaciones: number = 0
 
-  ngOnInit(){
+  constructor (private router: Router) {}
+
+  ngOnInit () {
     this.notificaciones = [
-
+      {
+        id: 1,
+        mensaje:
+          'Usted ha sido invitado a un evento! Toque aquí para ver mas detalles',
+        fechaRecibida: new Date('2023-10-12 10:25')
+      },
+      {
+        id: 2,
+        mensaje: 'El evento formal "Entrega de diplomas" comenzará pronto',
+        fechaRecibida: new Date('2023-12-14 15:32')
+      },
+      {
+        id: 3,
+        mensaje: 'Notificación 3',
+        fechaRecibida: new Date('2023-12-30 21:19')
+      },
+      {
+        id: 4,
+        mensaje: 'Notificación 4',
+        fechaRecibida: new Date('2023-12-31 08:44')
+      },
+      {
+        id: 5,
+        mensaje: 'Notificación 5',
+        fechaRecibida: new Date('2023-12-31 08:44')
+      },
+      {
+        id: 6,
+        mensaje: 'Notificación 6',
+        fechaRecibida: new Date('2023-12-31 08:44')
+      },
+      {
+        id: 7,
+        mensaje: 'Notificación 7',
+        fechaRecibida: new Date('2023-12-31 08:44')
+      },
+      {
+        id: 8,
+        mensaje: 'Notificación 8',
+        fechaRecibida: new Date('2023-12-31 08:44')
+      }
     ]
+
+    this.contadorNotificaciones = this.notificaciones.length
   }
 }
 
 interface Notificacion {
-  id: number;
-  mensaje: string;
-  fechaRecibida: string;
+  id: number
+  mensaje: string
+  fechaRecibida: Date
 }
